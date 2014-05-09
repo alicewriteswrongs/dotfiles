10cb5c5981705bc488a7d6455e9aea45bbe55daa
Benjamin Pote <ben.pote@gmail.com>
2014-05-08 21:09:13 -0400

    Moar sup config!

--- sup/hooks/before-add-message.rb
+++ sup/hooks/before-add-message.rb
@@ -11,7 +11,7 @@ if subj.start_with? '[tonics]'
 end
 
 if subj.start_with? 'arch-general'
-    message.add_lavel :arch-general
+    message.add_label :arch-general
 end
 
 if subj.start_with? '[sup-talk'
@@ -21,3 +21,36 @@ end
 if subj.start_with? '[compsci]'
     message.add_label :hampcompsci
 end
+
+#sender based labels
+if message.from.email.start_with? 'ryanvcostello@gmail.com'
+    message.add_label :friends
+end
+
+if message.from.email.start_with? 'leocelo@gmail.com'
+    message.add_label :friends
+end
+
+
+if message.from.email.start_with? 'ahgardiner@gmail.com'
+    message.add_label :friends
+end
+
+
+if message.from.email.start_with? 'ken.pote@gmail.com'
+    message.add_label :family
+end
+
+
+if message.from.email.start_with? 'epote12@gmail.com'
+    message.add_label :family
+end
+
+
+if message.from.email.start_with? 'janeself123@gmail.com'
+    message.add_label :friends
+end
+
+
+
+
