ed81f0a98717768d625e6cf79e5b9a208c870c13
Alice <alice.writes.wrongs@gmail.com>
2015-05-11 19:01:07 -0400

    Axyridis sig updated

--- sup/hooks/signature.rb
+++ sup/hooks/signature.rb
@@ -6,7 +6,7 @@ if from_email == 'ben.pote@gmail.com'
 Public PGP key: https://pgp.mit.edu/pks/lookup?op=get&search=0xB3BAFECBA7AFC2EF
 PGP fingerprint: 695B 9B5D A910 B280 92D1 360E B3BA FECB A7AF C2EF"
 elsif from_email == 'axyridis@riseup.net'
-    "AR
+    "~*~ alice ~*~
 Public PGP key: https://pgp.mit.edu/pks/lookup?op=get&search=0x3D47670F42269A83
 PGP fingerprint: 43D1 4BEF 39F7 4027 B02A DC4D 3D47 670F 4226 9A83"
 elsif from_email == 'alice.writes.wrongs@gmail.com'
