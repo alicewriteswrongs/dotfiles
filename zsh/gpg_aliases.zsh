8465e81005f5d18048bc6d9dbe3bdac638ce8392
Alice <alice.writes.wrongs@gmail.com>
2015-05-29 23:08:32 -0400

    aliases for GPG command line interface

---
+++ zsh/gpg_aliases.zsh
@@ -0,0 +1,4 @@
+alias gpgrefresh='gpg --refresh-keys --keyserver hkps://pool.sks-keyservers.net'
+alias gpgupload='gpg --send-keys --keyserver hkps://pool.sks-keyservers.net'
+alias gpglist='gpg --list-keys'
+alias gpgfing='gpg --fingerprint'
