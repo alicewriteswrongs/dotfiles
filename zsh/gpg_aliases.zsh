6864cef1a25cae209dbb010992bb0b31194142be
Alice <alice.writes.wrongs@gmail.com>
2015-05-30 23:39:42 -0400

    Arch y u no support hkps

--- zsh/gpg_aliases.zsh
+++ zsh/gpg_aliases.zsh
@@ -1,4 +1,4 @@
-alias gpgrefresh='gpg --refresh-keys --keyserver hkps://pool.sks-keyservers.net'
+alias gpgrefresh='gpg --refresh-keys --keyserver hkp://pool.sks-keyservers.net'
 alias gpgupload='gpg --send-keys --keyserver hkps://pool.sks-keyservers.net'
 alias gpglist='gpg --list-keys'
 alias gpgfing='gpg --fingerprint'
