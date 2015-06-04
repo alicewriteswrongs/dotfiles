c09cfe296a638c6bd567016c981b9ec9f6c84356
Alice <alice.writes.wrongs@gmail.com>
2015-06-04 13:53:49 -0400

    Kernel stuff, tmux is better (batt, mpd)

--- zsh/kernel_aliases.zsh
+++ zsh/kernel_aliases.zsh
@@ -1,2 +1,4 @@
 # some aliases for dealing with kernel compilation!
 alias cleankernel='make mrproper' # remove previous compilation
+alias quemudefault='qemu-system-i386  -m 512 -net nic -net user,hostfwd=tcp:127.0.0.1:2222-:22 vm-cs155/vm-cs155.vmdk'
+
