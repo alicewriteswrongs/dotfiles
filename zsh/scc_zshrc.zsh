47e1b3a028873cf0ac6c7e03caeed486ae516fed
Alice <alice.writes.wrongs@gmail.com>
2015-06-11 09:03:37 -0400

    More cleaning! Better!

--- zsh/scc_zshrc.zsh
+++
@@ -1,8 +0,0 @@
-source ~/Code/dotfiles/zsh/general_config
-source ~/Code/dotfiles/zsh/dotfile_aliases
-source ~/Code/dotfiles/zsh/coreutils_aliases
-source ~/Code/dotfiles/zsh/pacman_aliases
-source ~/Code/dotfiles/zsh/git_aliases
-source ~/Code/dotfiles/zsh/ssh_aliases
-source ~/Code/dotfiles/zsh/scc_aliases
-source ~/Code/dotfiles/zsh/keybind_config
