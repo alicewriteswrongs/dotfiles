58417be0e573288ef3fdf247fbbe3b890b39babf
Alice <alice.writes.wrongs@gmail.com>
2015-06-11 09:03:37 -0400

    More cleaning! Better!

--- zsh/scc_aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias jobs='qstat -u benpote'
-alias donejobs='qacct -d 7 -o benpote -j'
-alias space='pquota -u mullenl'
-alias mullenl='cd /projectnb/mullenl/'
-alias R='~/R_install/R-3.1.1/bin/R'
-alias mupdf='/projectnb/mullenl/programs2/mupdf-1.5-source/build/debug/mupdf-x11'
-
--- zsh/scc_zshenv.zsh
+++
@@ -1,7 +0,0 @@
-typeset -U path
-path=(/projectnb/mullenl/programs/ /projectnb/mullenl/programs2/platanus_1.2.1/ /projectnb/mullenl/programs2/rsem-1.2.15/ $path)
-
-export CLICOLOR=1
-export LSCOLORS=GxFxCxDxBxegedabagaced
-
-export EDITOR="vim"
