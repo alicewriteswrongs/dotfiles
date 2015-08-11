7ab8b866a5d6c8dee5cd060ea283176662d01881
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-11 12:01:01 -0400

    Link to keybase instead of fingerprint

--- sup/hooks/signature.rb
+++ sup/hooks/signature.rb
@@ -6,10 +6,10 @@ if from_email == 'ben.pote@gmail.com'
 PGP fingerprint: 695B 9B5D A910 B280 92D1 360E B3BA FECB A7AF C2EF"
 elsif from_email == 'axyridis@riseup.net'
     "~*~ alice ~*~
-PGP fingerprint: 43D1 4BEF 39F7 4027 B02A DC4D 3D47 670F 4226 9A83"
+https://keybase.io/aliceriot"
 elsif from_email == 'alice.writes.wrongs@gmail.com'
     "~*~ alice ~*~
-PGP fingerprint: 43D1 4BEF 39F7 4027 B02A DC4D 3D47 670F 4226 9A83"
+https://keybase.io/aliceriot"
 else
     "~*~ alice ~*~"
 end
