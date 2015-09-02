0ac1fc3c571d9cc77dd18c19f578a2e949aeaffe
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-02 10:33:08 -0400

    automate rebasing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias abcdehighqual='abcde -o ogg:"-q 7"' #rips at ogg vorbis quality level 7
+alias grebi='git checkout master && git pull origin master && git rebase -i master'
 alias s3down='aws s3 sync'
 alias vssh='vagrant ssh'
 alias vup='vagrant up'
