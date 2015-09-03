5c30c7b625169e1d47c6864aa35b92c93962c4e3
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-03 13:22:04 -0400

    12 -> 24

--- zsh/general_config.zsh
+++ zsh/general_config.zsh
@@ -30,7 +30,7 @@ autoload -U colors && colors
 ## looks like alice <3 dir         current_time
 PROMPT=" %{$fg[blue]%}%n%{$reset_color%} %{$fg[magenta]%}<3 %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}"
 #PROMPT="[%{$fg[green]%}%n%{$reset_color%}@%{$fg[blue]%}%m] %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}%# " #old version!
-RPROMPT="%{$fg_no_bold[blue]%}%t%{$reset_color%} "
+RPROMPT="%{$fg_no_bold[blue]%}%T%{$reset_color%} "
 #♥
 
 
