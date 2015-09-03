fde9963188725c458327052056ab65e3d761b9ec
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-03 16:10:45 -0400

    making this better

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
-#you can try to run this, or use it as a setup guide
+echo "you can try to run this, or use it as a setup guide"
+echo "the dotfiles repo should already be cloned in ~/Code/dotfiles"
+echo "install packages"
+sudo apt-get install vim-gnome python-dev python3-dev tmux zsh build-essential cmake node-legacy npm silversearcher-ag htop chromium mpd git ruby rvm python-pip python3-pip
 
-sudo apt-get install vim-gnome python-dev python3-dev tmux zsh build-essential cmake node-legacy npm silversearcher-ag htop
+echo "pull in git submodules"
 cd ~/Code/dotfiles && git submodule init && git submodule update
-cd ~/Code/dotfiles/
+
+echo "change shell to zsh"
+chsh $(whoami) &&
+
+echo "symlink dotfiles"
 rm ~/.vimrc
 ln -s ~/Code/dotfiles/vimrc/vimrc ~/.vimrc
 rm ~/.zshrc && rm ~/.zshenv
 ln -s ~/Code/dotfiles/zsh/zshrc.zsh ~/.zshrc
 ln -s ~/Code/dotfiles/zsh/zshenv.zsh ~/.zshenv
-chsh $(whoami)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "run 'PluginInstall'"
-vim && python ~/.vim/bundle/youcompleteme/install.py
 ln -s ~/Code/dotfiles/tmux/tmux.conf ~/.tmux.conf
 rm ~/.gitconfig && ln -s ~/Code/dotfiles/git/.gitconfig ~/.gitconfig
+
+echo "get vim plugins"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "USER INTERVENTION: run 'PluginInstall'" && 
+vim && python ~/.vim/bundle/youcompleteme/install.py
+
+# install solarized colors for gnome-terminal
+cd ~/Code/dotfiles/gnome-terminal-colors-solarized/ && ./install.sh
+
+echo "generate an ssh keypair with"
+echo ""
+echo " ssh-keygen -t rsa -b 4096 -C 'email@domain.org'"
+echo ""
+echo "remember to add it to github!"
+
+echo "you're done!"
