31d8d479e0ec3e115414fcd4ef3853fabad8ed2b
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-15 09:53:25 -0400

    I changed a thing!

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,20 +1,20 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "/home/alice/Code/dotfiles/fzf/bin" ]]; then
-  export PATH="$PATH:/home/alice/Code/dotfiles/fzf/bin"
+if [[ ! "$PATH" =~ "/home/alice/Code/dotfiles/extras/fzf/bin" ]]; then
+  export PATH="$PATH:/home/alice/Code/dotfiles/extras/fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" =~ "/home/alice/Code/dotfiles/fzf/man" && -d "/home/alice/Code/dotfiles/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/alice/Code/dotfiles/fzf/man"
+if [[ ! "$MANPATH" =~ "/home/alice/Code/dotfiles/extras/fzf/man" && -d "/home/alice/Code/dotfiles/extras/fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/alice/Code/dotfiles/extras/fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "/home/alice/Code/dotfiles/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- =~ i ]] && source "/home/alice/Code/dotfiles/extras/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/alice/Code/dotfiles/fzf/shell/key-bindings.zsh"
+source "/home/alice/Code/dotfiles/extras/fzf/shell/key-bindings.zsh"
 
