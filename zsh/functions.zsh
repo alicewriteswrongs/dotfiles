e1e8f6d2748469ce6e030b10224553e71ccbd3e6
Alice Pote <alice.writes.wrongs@gmail.com>
2015-10-05 12:20:06 -0400

    keybind for fzf git branch picker

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,20 @@ function selectbranch() {
     git branch | fzf
 }
 
-function gs {
+function gs() {
     git commit -a -m "WIP"
     git checkout $(selectbranch)
+    zle redisplay
+}
+
+zle -N gs
+bindkey '^b' gs
+
+function wrebase() {
+    git commit -a -m 'wip'
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    git checkout master
+    git pull origin master
+    git checkout $branch
+    git rebase -i master
 }
