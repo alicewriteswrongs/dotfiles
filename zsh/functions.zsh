58beeee22ce5fad57bac47ec172ae5f483ef7bf5
Alice Pote <alice.writes.wrongs@gmail.com>
2015-10-06 09:21:22 -0400

    nuke branch function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,7 @@ function selectbranch() {
     git branch | fzf
 }
 
-function gs() {
+function gs() { # use fzf to pick branches
     git commit -a -m "WIP"
     git checkout $(selectbranch)
     zle redisplay
@@ -10,6 +10,19 @@ function gs() {
 
 zle -N gs
 bindkey '^b' gs
+
+function gbnuke() { # nuke the current branch or $1
+    if [[ $1 ]]; then
+        git checkout master
+        git branch -D $1
+        git push origin :$1
+    else
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        git checkout master
+        git branch -D $branch
+        git push origin :$branch
+    fi
+}
 
 function rebase() {
     git commit -a -m 'WIP'
@@ -19,4 +32,3 @@ function rebase() {
     git checkout $branch
     git rebase -i master
 }
-
