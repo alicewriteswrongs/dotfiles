ad4a130340a3dc8e4aebbe28ad185905ac1f2c39
Alice Pote <alice.writes.wrongs@gmail.com>
2015-10-30 15:31:40 -0400

    function for creating a nice tmux instance

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -139,3 +139,14 @@ function rebdiff() { # rebase and reload diff
     git diff origin/master
 }
 
+function new() {
+    session_name=$1
+    tmux new-session -s $session_name -n 1 -d
+
+    for i in {2..10}
+    do
+        tmux new-window -t $session_name -n $i
+    done
+
+    tmux attach-session -t $session_name
+}
