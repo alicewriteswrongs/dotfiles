6021f4ed6b87a5b530cd8c408eb7d96efa059cf4
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-11 09:26:03 -0500

    install ruby with rvm not apt

--- install.sh
+++ install.sh
@@ -83,7 +83,11 @@ function install_ruby() {
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     \curl -sSL https://get.rvm.io | bash -s stable
-    sudo apt-get -y install ruby rbenv
+    source ~/.rvm/scripts/rvm
+    rvm install ruby-2.2.3
+    rvm use 2.2.3
+    gem install bundler
+    rvm rvmrc warning ignore allGemfiles
 }
 
 #python things
