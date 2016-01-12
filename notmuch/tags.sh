df1986ce4452ad129d01ebdfa17ffaa94276f403
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-12 16:50:36 -0500

    more mailing lists and stuffffff

--- notmuch/tags.sh
+++ notmuch/tags.sh
@@ -4,6 +4,7 @@
 notmuch tag +LPC -- subject:"[lpc-c]"
 notmuch tag +crypto -- subject:"[Cryptography]"
 notmuch tag +crypto -- to:cryptography@metzdowd.com
+notmuch tag +crypto -- to:messaging@moderncrypto.org
 notmuch tag +sup -- subject:"[sup]"
 notmuch tag +tor -- subject:"[tor-talk]"
 notmuch tag +tor -- to:tor-talk@lists.torproject.org
@@ -18,6 +19,12 @@ notmuch tag +BATS -- to:boston-area-trans-support@yahoogroups.com
 notmuch tag +Julia -- to:julia-users@googlegroups.com
 notmuch tag +kernel_newbies -- to:kernelnewbies@kernelnewbies.org
 notmuch tag +Arch to:arch-general@archlinux.org
+notmuch tag +blackandpink to:blackandpinkboston-courtsupport@googlegroups.coms
+notmuch tag +nylug to:nylug-talk@nylug.org
+
+# notifications and so on
+notmuch tag +github_notification from:notifications@github.com
+notmuch tag +EFF from:editor@eff.org
 
 # skip the inbox
 function skip_inbox_from () {
@@ -40,3 +47,7 @@ skip_inbox_from kernelnewbies@kernelnewbies.org
 skip_inbox_from noreply@newrelic.com
 skip_inbox_from notifier@qbox.io
 skip_inbox_from notifications@myprewards.com
+skip_inbox_from notifications@github.com
+skip_inbox_from cloudsuspensions@trustedproxies.com
+skip_inbox_to *@cic.us
+skip_inbox_to julia-users@googlegroups.com
