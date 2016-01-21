7dd2cf3f00e0612c8b632f90414a2eaedf6aa02c
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-21 16:45:28 -0500

    I continue to not understand zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -56,9 +56,7 @@ fhash() {
   echo -n $(echo "$commit" | sed "s/ .*//")
 }
 
-function vdo() {
-    vagrant ssh -c '$*'
-}
+function vdo { vagrant ssh -c "$*" }
 
 function gd() {
     if [[ $1 ]]; then
