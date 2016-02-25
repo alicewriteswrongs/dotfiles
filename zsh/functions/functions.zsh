221e1fe2d58c73372ddb95fd81e374d3cd6f6fe7
Alice Pote <alice.writes.wrongs@gmail.com>
2016-02-25 15:34:30 -0500

    move fzf and some dev related functions out

--- zsh/functions/functions.zsh
+++ zsh/functions/functions.zsh
@@ -1,49 +1,7 @@
 # fzf functions
 source ~/dotfiles/zsh/functions/github.zsh
 source ~/dotfiles/zsh/functions/git.zsh
-
-function find_within_files() {
-    ag --nobreak --noheading . | fzf-tmux
-    zle redisplay
-}
-
-zle -N find_within_files
-bindkey '^f' find_within_files
-
-function select_directory() {
-    find ${1:-.} -type d 2> /dev/null | fzf-tmux 
-}
-
-function fancy_cd() {
-    cd $(select_directory)
-    zle redisplay
-}
-
-zle -N fancy_cd
-bindkey '^P' fancy_cd
-
-fkill() {
-  pid=$(ps -ef | sed 1d | fzf-tmux -m | awk '{print $2}')
-
-  if [ "x$pid" != "x" ]
-  then
-    kill -${1:-9} $pid
-  fi
-}
-
-fcommit() {
-  local commits commit
-  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf-tmux --tac +s +m -e) &&
-  git checkout $(echo "$commit" | sed "s/ .*//")
-}
-
-fhash() {
-  local commits commit
-  commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf-tmux --tac +s +m -e --ansi --reverse) &&
-  echo -n $(echo "$commit" | sed "s/ .*//")
-}
+source ~/dotfiles/zsh/functions/fzf.zsh
 
 function vdo { ssh default "/bin/bash -l -c '$*'" }
 
@@ -93,19 +51,6 @@ function latex_to_markdown() {
 function sscreen() {
     scrot -s ~/screens/$1
 }
-
-function vreinstall() {
-    sudo modprobe vboxdrv
-    sudo /etc/init.d/virtualbox start
-}
-
-function npm_reset() {
-    rm -r node_modules
-    npm cache clean
-    npm install
-}
-
-# improved WIP commits
 
 function zip_it() {
     if [[ -f $1.zip ]]; then
---
+++ zsh/functions/fzf.zsh
@@ -0,0 +1,44 @@
+function find_within_files() {
+    ag --nobreak --noheading . | fzf-tmux
+    zle redisplay
+}
+
+zle -N find_within_files
+bindkey '^f' find_within_files
+
+function select_directory() {
+    find ${1:-.} -type d 2> /dev/null | fzf-tmux 
+}
+
+function fancy_cd() {
+    cd $(select_directory)
+    zle redisplay
+}
+
+zle -N fancy_cd
+bindkey '^P' fancy_cd
+
+fkill() {
+  pid=$(ps -ef | sed 1d | fzf-tmux -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    kill -${1:-9} $pid
+  fi
+}
+
+fcommit() {
+  local commits commit
+  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf-tmux --tac +s +m -e) &&
+  git checkout $(echo "$commit" | sed "s/ .*//")
+}
+
+fhash() {
+  local commits commit
+  commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf-tmux --tac +s +m -e --ansi --reverse) &&
+  echo -n $(echo "$commit" | sed "s/ .*//")
+}
+
+
