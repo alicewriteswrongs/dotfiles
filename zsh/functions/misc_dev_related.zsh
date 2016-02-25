fbd0264e53c9917f650df265005f4d0f4eac1373
Alice Pote <alice.writes.wrongs@gmail.com>
2016-02-25 15:34:30 -0500

    move fzf and some dev related functions out

---
+++ zsh/functions/misc_dev_related.zsh
@@ -0,0 +1,32 @@
+function vreinstall() {
+    sudo modprobe vboxdrv
+    sudo /etc/init.d/virtualbox start
+}
+
+function npm_reset() {
+    rm -r node_modules
+    npm cache clean
+    npm install
+}
+
+function vdo { ssh default "/bin/bash -l -c '$*'" }
+
+function crun() {
+    if [[ ! $1 ]]; then
+        echo "no target supplied, just building"
+        make
+    elif [[ -f $1 && $1 = *.c ]]; then
+        target=$(echo $1 | sed 's/\(.*\)\..*/\1/')
+        if [[ -f $target ]]; then
+            rm $target
+        fi
+        make $target
+        ./$target
+    else
+        if [[ -f $1 ]]; then
+            rm $1
+        fi
+        make $1
+        ./$1
+    fi
+}
