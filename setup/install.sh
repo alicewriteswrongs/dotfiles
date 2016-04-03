ae99535d27de69e8422a059ad11b0c6acd7bf76f
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-03 09:34:49 -0400

    add python-dev

--- setup/install.sh
+++ setup/install.sh
@@ -30,7 +30,7 @@ sudo service nfs-kernel-server start
 
 # install various langauge runtimes and build tools
 sudo apt -y install golang-go
-sudo apt -y install python3-dev python-pip python3-pip python-virtualenv python-setuptools python3-setuptools
+sudo apt -y install python3-dev python-dev python-pip python3-pip python-virtualenv python-setuptools python3-setuptools
 sudo apt -y install build-essential autoconf automake libtool cmake pkg-config
 sudo apt -y install libboost1.55-dev-all libboost1.55-dev libboost1.55-tools-dev scons libgdbm libgdbm-dev libncurses5 libncurses5-dev bison libreadline6 libreadline6-dev libyaml libyaml-dev libsqlite3 libsqlite3-dev sqlite3 libffi libffi-dev libpq libpq-dev notmuch libnotmuch3 libnotmuch-dev libglibmm-2.4-dev libgtkmm-3.0-1 libgtkmm-3.0-dev libgmime-2.6-0 libgmime-2.6-dev libwebkitgtk-3.0-0 libwebkitgtk-3.0-dev libwebkit-dev g++
 
