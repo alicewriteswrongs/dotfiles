b99579c7a507a473ca07f3811606d76e2108d981
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-05 17:49:46 -0400

    make working with docker a little nicer

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,9 @@
 alias -g G='| grep'
 alias elmserve='elm-reactor -p 8080'
+alias drun='docker-compose run web'
+alias dpshell='docker-compose run web ./manage.py shell'
+alias dup='docker-compose up'
+alias dhalt='docker-compose stop'
 alias pdfs='cd ~/SpiderOak\ Hive/PDFs'
 alias abcdehighqual='abcde -o ogg:"-q 7"' #rips at ogg vorbis quality level 7
 alias abig='tmux attach -t big'
