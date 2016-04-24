5a37170ff45f8a282e387726c7d32ba055788f76
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-24 08:40:08 -0400

    gnome-terminal -> xfce4

--- zsh/functions/x_windows.zsh
+++ zsh/functions/x_windows.zsh
@@ -2,6 +2,6 @@ function title {
    echo -ne "\033]0;$1\007"
 }
 
-function floating_gnome_term {
-    gnome-terminal -e "zsh -c '$(declare -f title); title Float; zsh'"
+function floating_xfce4_term {
+    xfce4-terminal -e "zsh -c '$(declare -f title); title Float; zsh'"
 }
