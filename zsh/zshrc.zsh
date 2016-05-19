5ee6ab1193815ec14ae2b9f8cf190966fa2bfe7d
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-19 09:01:13 -0400

    add heroku thingy

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -136,6 +136,7 @@ export PATH="$PATH:/home/alice/bin/turtl"
 export PATH="$PATH:/home/alice/bin/android-studio/bin"
 export PATH="$PATH:/home/alice/dotfiles/tmux/battery/bin"
 export PATH="$PATH:/home/alice/.gem/ruby/2.3.0/bin"
+export PATH="$PATH:/usr/local/heroku/bin"
 
 export NVM_DIR="/home/alice/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
