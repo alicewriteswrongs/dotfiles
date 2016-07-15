be21d57628521401c2d88cfa192eb293116ecf35
Alice Pote <alice.writes.wrongs@gmail.com>
2016-07-15 17:07:41 -0400

    some functions to automate upgrading npm packages

--- zsh/functions/misc_dev_related.zsh
+++ zsh/functions/misc_dev_related.zsh
@@ -38,3 +38,21 @@ function gorun() {
 function freplace() {
     ag -l $1 | xargs sed -i '' -e $2
 }
+
+function docker_js_upgrade() {
+    docker-compose run watch npm install --save $1@latest
+}
+
+function get_package_name() {
+    echo "$1" | sed 's/ .*//g'
+}
+
+function docker_js_upgrade_all() {
+    IFS=$'\n'
+    for package in $(npm outdated | tail -n +2); do
+        to_upgrade=$(get_package_name $package)
+        echo "upgrading $to_upgrade"
+        docker_js_upgrade $to_upgrade
+    done
+}
+
