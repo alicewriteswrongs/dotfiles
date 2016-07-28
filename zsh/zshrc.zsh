c67c91c515d78f4f9d432cb291cabd63ecb0203a
Alice Pote <alice.writes.wrongs@gmail.com>
2016-07-28 10:45:21 -0400

    some config changes

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -122,7 +122,7 @@ export NVIM_TUI_ENABLE_TRUE_COLOR=1
 
 
 [ -f ~/dotfiles/zsh/fzf.zsh ] && source ~/dotfiles/zsh/fzf.zsh
-[ -d ~/.rvm/ ] && source ~/.rvm/scripts/rvm
+# [ -d ~/.rvm/ ] && source ~/.rvm/scripts/rvm
 export LELLO_SYNC_PROVIDER=""
 export GOPATH="$HOME/Code/gospace"
 export PATH="$PATH:$GOPATH/bin"
