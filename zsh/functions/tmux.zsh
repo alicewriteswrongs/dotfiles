ee2e66a5c40cb41e663e90415e7422d4fc24d06e
Alice Pote <alice.writes.wrongs@gmail.com>
2016-10-07 10:39:54 -0400

    reviewing my tmux functions

--- zsh/functions/tmux.zsh
+++ zsh/functions/tmux.zsh
@@ -1,14 +1,12 @@
 
 function new() {
     session_name=$1
-    tmux new-session -s $session_name -n 1 -d
-
-    for i in {2..10}
-    do
-        tmux new-window -t $session_name -n $i
-    done
-
+    tmux new-session -s $session_name -n code -d
+    tmux new-window -t $session_name -n app
     tmux attach-session -t $session_name
 }
 
-
+function new_non_prog() {
+    session_name=$1
+    tmux new-session -s $session_name -n 1 -d
+}
