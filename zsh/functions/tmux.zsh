0454587a2eefa110f9d93a10586b5b0e297b6a85
Alice Pote <alice.writes.wrongs@gmail.com>
2017-03-20 13:25:35 -0400

    I changed some stuff, ran the newer version of astroid

--- zsh/functions/tmux.zsh
+++ zsh/functions/tmux.zsh
@@ -1,9 +1,6 @@
 
 function new() {
-    session_name=$1
-    tmux new-session -s $session_name -n code -d
-    tmux new-window -t $session_name -n app
-    tmux attach-session -t $session_name
+    tmux new -s $1
 }
 
 function new_non_prog() {
