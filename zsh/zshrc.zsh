e26d07dde4269e4baf099bd1b3a61159ecb89285
Alice Pote <alice.writes.wrongs@gmail.com>
2022-04-11 16:46:40 -0400

    fixes for apple silicon

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -118,8 +118,8 @@ export CDPATH="/Users/alice/Code:/Users/alice:."
 
 source ~/dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-source /usr/local/opt/fzf/shell/key-bindings.zsh
-source /usr/local/opt/fzf/shell/completion.zsh
+source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
+source /opt/homebrew/opt/fzf/shell/completion.zsh
 
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
